5f36c3a6d6a3066363de1178f46679b73e553b6e
gtkarthikeyan26 <gtkarthikeyan@gmail.com>
2025-02-25 16:36:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Quiz from './components/Quiz'
+
+export default function () {
+  return (
+    <div>
+        <Header />
+        <main><Quiz /></main>
+        
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logoImg from '../assets/quiz-logo.png'
+
+export default function Header() {
+  return (
+    <header>
+        <img src={logoImg} alt="quiz-logo"/>
+        <h1>ReactQuiz</h1>
+    </header>
+  )
+}
